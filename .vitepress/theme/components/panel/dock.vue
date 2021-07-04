12d0559ed210cbb8272934f08e5ab869a20c2976
davay42 <fond@fondfrukt.ru>
2021-07-04 14:41:48 +0300

    fav

--- .vitepress/theme/components/panel/dock.vue
+++ .vitepress/theme/components/panel/dock.vue
@@ -15,9 +15,9 @@ const props = defineProps({
 
 <style scoped>
 .panel {
-  @apply flex p-1 flex-col shadow items-center duration-300 fixed right-4 bottom-2  rounded-full transition-all opacity-50 bg-dark-900/30 dark:bg-dark-900 hover:(shadow-xl opacity-100);
+  @apply flex p-1 gap-4 md:gap-2 flex-col shadow items-center duration-300 fixed right-4 bottom-2  rounded-full transition-all opacity-50 bg-dark-900/30 dark:bg-dark-900 hover:(shadow-xl opacity-100);
 }
 .btn {
-  @apply outline-none duration-100 px-4 py-4 rounded-full transition-all transform  bg-light-100/10  hover:(bg-opacity-70);
+  @apply outline-none duration-100 p-2 md:(p-4) rounded-full transition-all transform  bg-light-100/10  hover:(bg-opacity-70);
 }
 </style>
