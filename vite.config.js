259f2f7811037273ac9579c6fb0a5f7c38424564
davay42 <fond@fondfrukt.ru>
2021-08-29 22:51:58 +0300

    bump and unplugins

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from 'vite'
 import Components from 'vite-plugin-components'
-import Icons, { ViteIconsResolver } from 'vite-plugin-icons'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 import WindiCSS from 'vite-plugin-windicss'
 import { ViteAliases } from 'vite-aliases'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue\??/, // .vue
+      ],
+      imports: ['vue'],
+    }),
     svgLoader({
       svgo: false,
     }),
@@ -21,7 +30,7 @@ export default defineConfig({
       globalNamespaces: ['global'],
       customLoaderMatcher: (id) => id.endsWith('.md'),
       customComponentResolvers: [
-        ViteIconsResolver({
+        IconsResolver({
           componentPrefix: '',
         }),
       ],
